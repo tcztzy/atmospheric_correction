e255aa1640bd8eb5d6bc606047014531b62c6d59
Tang Ziya <tcztzy@gmail.com>
2020-02-09 13:06:07 +0800

    use poetry as project manager

--- zac/__init__.py
+++ zac/__init__.py
@@ -1,5 +1,13 @@
-from .zac_s2 import zac_s2
-from .zac_l8 import zac_l8
-from .the_aerosol import solve_aerosol
-from .the_correction import atmospheric_correction
-from .atmo_solver import solving_atmo_paras
+import os.path
+from importlib.metadata import PackageNotFoundError, version
+from os.path import expanduser
+
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+HOME = expanduser("~")
+DEFAULT_MCD43_DIR = os.path.join(HOME, "MCD43", "")
+DEFAULT_MCD43_VRT_DIR = os.path.join(HOME, "MCD43_VRT", "")
